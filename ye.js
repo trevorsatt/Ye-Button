ea801925c09d9c6f809e0012eef7ecfaaaaf35d6
Trevor Satterwhite <trevorsatterwhite@Trevors-MacBook-Pro.local>
2017-07-20 19:17:39 -0500

    update

--- ye.js
+++ ye.js
@@ -2,13 +2,28 @@ var audio = new Audio();
 var playlist = ["sound/song1.mp3", "sound/song2.mp3", "sound/song3.mp3","sound/song4.mp3","sound/song5.mp3","sound/song6.mp3"];
 
 
-
+var lastSong = 100;
 $(document).ready(function() {
   $(".btn").click(function(){
- 	var songNum = Math.floor(Math.random() * 6) + 0;
- 	console.log(songNum);
- 	audio.src = playlist[songNum];
+	
+	var choice = GetSong(lastSong);
+
+ 	console.log(choice);
+ 	audio.src = playlist[choice];
 	audio.play();
   });
 });
+
+function GetSong(last){
+	var songNum = Math.floor(Math.random() * 6) + 0;
+
+ 	if(last !== songNum && typeof songNum !== undefined){
+		lastSong = songNum;
+
+		return songNum;
+ 	} 
+ 	else {
+ 		GetSong(lastSong);
+ 	}
+}
 

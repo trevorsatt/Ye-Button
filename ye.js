f95352675e6259a215164ca340bf8b7db8cd1712
Trevor Satterwhite <trevorsatterwhite@Trevors-MBP.attlocal.net>
2017-10-16 22:28:58 -0500

    updated mobile button

--- ye.js
+++ ye.js
@@ -4,7 +4,7 @@ var playlist = ["sound/song1.mp3", "sound/song2.mp3", "sound/song3.mp3","sound/s
 
 var lastSong = 100;
 $(document).ready(function() {
-  $(".btn").click(function(){
+  $(".butn").click(function(){
 	
 	var choice = GetSong(lastSong);
 
@@ -27,3 +27,31 @@ function GetSong(last){
  	}
 }
 
+var audio = new Audio();
+var playlist = ["sound/song1.mp3", "sound/song2.mp3", "sound/song3.mp3","sound/song4.mp3","sound/song5.mp3","sound/song6.mp3","sound/song7.mp3","sound/song8.mp3"];
+
+
+var lastSong = 100;
+$(document).ready(function() {
+  $(".btn").click(function(){
+	
+	var choice = GetSong(lastSong);
+
+ 	console.log(choice);
+ 	audio.src = playlist[choice];
+	audio.play();
+  });
+});
+
+function GetSong(last){
+	var songNum = Math.floor(Math.random() * 8) + 0;
+
+ 	if(last !== songNum && typeof songNum !== undefined){
+		lastSong = songNum;
+
+		return songNum;
+ 	} 
+ 	else {
+ 		GetSong(lastSong);
+ 	}
+}
